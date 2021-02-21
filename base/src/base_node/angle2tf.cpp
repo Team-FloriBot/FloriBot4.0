f6eb4c1002d5df4299a37039a775492151e3915c
AndPister <apister@gmx.de>
2021-02-21 11:28:00 -0800

    Abgabe Computergrafik und multimedia Base_controle und websocket

---
+++ base/src/base_node/angle2tf.cpp
@@ -0,0 +1,46 @@
+#include <ros/ros.h>
+#include <ros/time.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <base/Angle.h>
+
+void AngleCallback(const base::Angle::ConstPtr& msg);
+tf2_ros::TransformBroadcaster* TF;
+geometry_msgs::TransformStamped TFAngleMsg;
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "angle2tf");
+    ros::NodeHandle Nh;
+    TF = new tf2_ros::TransformBroadcaster();
+    ros::Subscriber AngleSubs=Nh.subscribe("/sensors/bodyAngle", 1, &AngleCallback);
+
+    ros::spin();
+
+    return 0;
+}
+
+void AngleCallback(const base::Angle::ConstPtr& msg)
+{  
+    tf2::Quaternion q; 
+    ROS_ERROR("%f",msg->angle);
+    TFAngleMsg.header.seq=msg->header.seq;
+    TFAngleMsg.child_frame_id="jointRear";
+    TFAngleMsg.header.frame_id="jointFront";
+    TFAngleMsg.header.stamp=ros::Time::now();
+
+
+    q.setRPY(0,0,msg->angle);
+    TFAngleMsg.transform.translation.x=0;
+    TFAngleMsg.transform.translation.y=0;
+    TFAngleMsg.transform.translation.z=0;
+
+    TFAngleMsg.transform.rotation.x=q.x();
+    TFAngleMsg.transform.rotation.y=q.y();
+    TFAngleMsg.transform.rotation.z=q.z();
+    TFAngleMsg.transform.rotation.w=q.w();
+
+    TF->sendTransform(TFAngleMsg);
+
+
+}
---
+++ base/src/drives/differential_drive.cpp
@@ -0,0 +1,91 @@
+#include "drives/differential_drive.h"
+
+kinematics::differentialDrive::differentialDrive(double axesLength, double wheelDiameter)
+{
+    setParam(axesLength, wheelDiameter);
+}
+
+kinematics::differentialDrive::differentialDrive()
+{
+    reset();
+}
+
+kinematics::differentialDrive::~differentialDrive()
+{
+}
+
+void kinematics::differentialDrive::reset()
+{
+    Pose_.theta=0;
+    Pose_.x=0;
+    Pose_.y=0;
+
+    WheelSpeed_.leftWheel=0;
+    WheelSpeed_.rightWheel=0;
+
+    TimeStamp_=ros::Time::now();
+}
+
+void kinematics::differentialDrive::setParam(double axesLength, double wheelDiameter)
+{
+    reset();
+    axesLength_=axesLength;
+    wheelDiameter_=wheelDiameter;
+    wheelCircumference_=2*M_PI*wheelDiameter_/2;
+}
+
+geometry_msgs::Pose2D kinematics::differentialDrive::estimateActualPose()
+{
+    //Declare temporary Variables
+    geometry_msgs::Pose2D estimatedPose;
+
+    double deltaTime=(ros::Time::now()-TimeStamp_).toSec();
+   
+    estimatedPose.x=Pose_.x+Speed_.linear.x+deltaTime+cos(Pose_.theta+0.5*Speed_.angular.z*deltaTime);
+    estimatedPose.y=Pose_.y+Speed_.linear.x+deltaTime+sin(Pose_.theta+0.5*Speed_.angular.z*deltaTime);
+    estimatedPose.theta=Pose_.theta+Speed_.angular.z*deltaTime;
+
+    return estimatedPose;
+}
+
+geometry_msgs::Pose2D kinematics::differentialDrive::forwardKinematics(DifferentialWheelSpeed WheelSpeed, ros::Time Timestamp)
+{
+    double deltaTime=(Timestamp-TimeStamp_).toSec();
+    TimeStamp_=Timestamp;
+
+    WheelSpeed_=WheelSpeed;
+    Speed_.linear.x=(WheelSpeed_.leftWheel*wheelCircumference_+WheelSpeed_.rightWheel*wheelCircumference_)/2;
+    Speed_.angular.z=(WheelSpeed_.rightWheel*wheelCircumference_-WheelSpeed_.leftWheel*wheelCircumference_)/axesLength_;
+
+    //from Papers and Books
+    Pose_.x+=Speed_.linear.x*deltaTime*cos(Pose_.theta+0.5*Speed_.angular.z*deltaTime);
+    Pose_.y+=Speed_.linear.x*deltaTime*sin(Pose_.theta+0.5*Speed_.angular.z*deltaTime);
+    Pose_.theta+=Speed_.angular.z*deltaTime;
+   
+
+    return Pose_;
+}
+
+
+kinematics::DifferentialWheelSpeed kinematics::differentialDrive::inverseKinematics(geometry_msgs::Twist cmdVelMsg)
+{
+    kinematics::DifferentialWheelSpeed WheelSpeed;
+
+    WheelSpeed.rightWheel=cmdVelMsg.linear.x+cmdVelMsg.angular.z*axesLength_/2;
+    WheelSpeed.leftWheel=WheelSpeed.rightWheel-cmdVelMsg.angular.z*axesLength_;
+
+    WheelSpeed.rightWheel=WheelSpeed.rightWheel/wheelDiameter_;
+    WheelSpeed.leftWheel=WheelSpeed.leftWheel/wheelDiameter_;
+
+    return WheelSpeed;
+}
+
+geometry_msgs::Pose2D kinematics::differentialDrive::getActualPose()
+{
+    return Pose_;
+}
+
+geometry_msgs::Twist kinematics::differentialDrive::getSpeed()
+{
+    return Speed_;
+}
