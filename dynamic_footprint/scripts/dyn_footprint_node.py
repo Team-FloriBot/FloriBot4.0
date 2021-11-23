530bff3e73c0083d71a8d50fbdca662feb6d0840
Moritz Emanuel Böker <moritz.boeker@gmx.de>
2021-11-23 18:04:32 +0100

    Problem: Navigation with switching the navigation base between axesFront and axesRear depending on the driving direction with switch_nav_base lead to oscillations and did not work. Solution: Use static base_link as navigation frame --> forward driving is done well wit dwa_local_planner, backwards driving capability is poor but works a little Note

--- dynamic_footprint/scripts/dyn_footprint_node.py
+++ dynamic_footprint/scripts/dyn_footprint_node.py
@@ -4,12 +4,13 @@
 # lookupTransform(): https://docs.ros2.org/foxy/api/tf2_ros/classtf2__ros_1_1Buffer.html
 # tf2TutorialsWriting a tf2 listener (Python): http://wiki.ros.org/tf2/Tutorials/Writing%20a%20tf2%20listener%20%28Python%29
 
+from inspect import signature
 import rospy
-from geometry_msgs.msg import Point, PointStamped
+from geometry_msgs.msg import Point, PointStamped, Twist
 import tf2_ros
 import tf2_geometry_msgs
 import numpy as np
-
+from enum import Enum
 import dynamic_reconfigure.client
 
 """ 
@@ -24,9 +25,7 @@ current tf between 'axesRear' and 'axesFront' via dynamic reconfigure.
 
 def transform_point(tf_src_targ, point_wrt_source):
     """ Transforms a point [[x, y, z]] from source to target frame. """
-    point_wrt_target = \
-        tf2_geometry_msgs.do_transform_point(PointStamped(point=point_wrt_source),
-            tf_src_targ).point
+    point_wrt_target = tf2_geometry_msgs.do_transform_point(PointStamped(point=point_wrt_source), tf_src_targ).point
     return [point_wrt_target.x, point_wrt_target.y, point_wrt_target.z]
 
 def transform_point_arr(point_arr, transform):
@@ -39,24 +38,26 @@ def transform_point_arr(point_arr, transform):
     return np.array(tf_point_arr)
 
 if __name__ == "__main__":
+
+    class LeadingCar(Enum):
+        FRONT = 1
+        REAR = 2
+    boss = LeadingCar.FRONT
     # constant footprint vertices of front carriage in frame 'axesFront'='base_link'
     footprint_front = [[-0.383,0.001, 0.0],
                         [-0.243,0.168, 0.0],
                         [0.277,0.168, 0.0],
                         [0.277,-0.168, 0.0],
-                        [-0.243,-0.168, 0.0],
-                        [-0.383,-0.001, 0.0]]
+                        [-0.243,-0.168, 0.0]]
     footprint_front = np.array(footprint_front)
 
     # constant footprint vertices of rear carriage in frame 'axesRear' --> transform to 'axesFront'
-    footprint_rear = [[0.243,-0.168, 0.0],
+    footprint_rear = [[0.383,-0.001, 0.0],
+                        [0.243,-0.168, 0.0],
                         [-0.277,-0.168, 0.0],
                         [-0.277,0.168, 0.0],
                         [0.243,0.168, 0.0]]
     footprint_rear = np.array(footprint_rear)
-
-    source_frame = 'axesRear'
-    target_frame = 'axesFront'
 
     rospy.init_node("dynamic_footprint_client", anonymous=True)
 
@@ -66,16 +67,26 @@ if __name__ == "__main__":
     client1 = dynamic_reconfigure.client.Client("/move_base/local_costmap", timeout=30)#, config_callback=dyn_cb)
     client2 = dynamic_reconfigure.client.Client("/move_base/global_costmap", timeout=30)#, config_callback=dyn_cb)
 
-
+    period = 1/rospy.get_param("~rate")
     rate = rospy.Rate(rospy.get_param("~rate"))
     footprint_padding = rospy.get_param("~footprint_padding")
     
     while not rospy.is_shutdown():
-        tf_buffer = tf2_ros.Buffer(rospy.Duration(2.0))
-        tf2_ros.TransformListener(tf_buffer)
+        if boss == LeadingCar.FRONT:
+            # if forward-driving: transform rear footprint into front frame
+            source_frame = 'axesRear'
+            target_frame = 'axesFront'
+            footprint_lead = footprint_front
+            footprint_follow = footprint_rear
+        else:
+            # if backward-driving: transform front footprint into rear frame
+            source_frame = 'axesFront'
+            target_frame = 'axesRear'
+            footprint_lead = footprint_rear
+            footprint_follow = footprint_front
 
         try:
-            tf_src_targ = tf_buffer.lookup_transform(target_frame, source_frame, rospy.Time(0), rospy.Duration(0.1))
+            tf_src_targ = tfBuffer.lookup_transform(target_frame, source_frame, rospy.Time(0), rospy.Duration(period))
         except tf2_ros.ConnectivityException:
             rospy.logerr('Tf tree between {:s} and {:s} is not connected'.format(source_frame, target_frame))
             rate.sleep
@@ -97,8 +108,8 @@ if __name__ == "__main__":
             rate.sleep
             continue
         else:
-            footprint_rear_wrt_front = transform_point_arr(footprint_rear, tf_src_targ)
-            footprint = np.concatenate((footprint_front, footprint_rear_wrt_front))
+            footprint_follow_wrt_lead = transform_point_arr(footprint_follow, tf_src_targ)
+            footprint = np.concatenate((footprint_lead, footprint_follow_wrt_lead))
             footprint_2d = footprint[:,:2] 
             footprint_2d_str = footprint_2d.tolist()
             client1.update_configuration({"footprint":footprint_2d_str, "footprint_padding":footprint_padding})
