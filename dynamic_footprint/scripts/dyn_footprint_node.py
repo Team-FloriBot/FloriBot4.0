0b7571431ef77688fb16f6998ccce397f92c7c66
Moritz Emanuel Böker <moritz.boeker@gmx.de>
2021-12-09 17:44:49 +0100

    Problem: Static robot footprint only works if driving forward. Solution: Added semi-static robot footprint that switches the front/ rear footprint when driving forwards/ backwards. Note

--- dynamic_footprint/scripts/dyn_footprint_node.py
+++ dynamic_footprint/scripts/dyn_footprint_node.py
@@ -15,8 +15,30 @@ import dynamic_reconfigure.client # for sending modified footprint
 from dynamic_reconfigure.server import Server # for choosing between dynamic and static footprint
 from dynamic_footprint.cfg import FootprintModeConfig
 """ 
-Updates the dynamic footprint of the floribot's front and rear carriage according to the
-current tf between 'axesRear' and 'axesFront' via dynamic reconfigure.
+Updates the footprint of the floribot's front and rear carriage according to settings in dynamic reconfigure:
+LEADING_CAR:                The carriage which is the head of the robot, either FRONT or REAR.
+                            The navigation frame (default: base_link) must be positioned at the specified HEAD.
+                            - if FRONT, then navigation frame must be front axle (identical to base_link)
+                            - if REAR, then navigation frame must be at rear axle (not implemented yet)
+FOOTPRINT_MODE:             What mode the footprint is to work in.
+Different combinations:     
+-----------------------------------------------------------------------------------------------------------------
+LEADING_CAR |   FOOTPRINT_MODE  |   footprint behavior                                                          |
+-----------------------------------------------------------------------------------------------------------------
+FRONT       |   STATIC          |   Only the footprint of the front carriage (which is static) is used,         |
+            |                   |   regardless of the driving direction.                                        |
+-----------------------------------------------------------------------------------------------------------------
+REAR        |   STATIC          |   Only the footprint of the rear carriage (which is static) is used,          |
+            |                   |   regardless of the driving direction.                                        |
+-----------------------------------------------------------------------------------------------------------------
+FRONT/ REAR |   SEMI_STATIC     |   When driving forwards: static front footprint is used.                      |
+            |                   |   When driving backwards: dynamic rear footprint is used depending on         |
+            |                   |   on current angle of articulation joint.                                     |
+-----------------------------------------------------------------------------------------------------------------
+FRONT/ REAR |   DYNAMIC         |   Both the static front footprint and the dynamic rear footprint are used     |
+            |                   |   depending on current angle of articulation joint and regardless of driving  |
+            |                   |   direction.                                                                  |
+-----------------------------------------------------------------------------------------------------------------
 """
 
 class LEADING_CAR(Enum):
@@ -25,12 +47,20 @@ class LEADING_CAR(Enum):
 
 class FOOTPRINT_MODE(Enum):
     STATIC = 0
-    DYNAMIC = 1
+    SEMI_STATIC = 1
+    DYNAMIC = 2
+
+class DRIVING_DIRECTION(Enum):
+    FORWARD = 0
+    BACKWARD = 1
 
 class FootprintMode():
     def __init__(self):
         self.leading_car = LEADING_CAR.FRONT
         self.footprint_mode = FOOTPRINT_MODE.STATIC
+        self.driving_direction = DRIVING_DIRECTION.FORWARD
+        self.update_local_footprint = True
+        self.update_global_footprint = False
 
         # constant footprint vertices of front carriage in frame 'axesFront'
         footprint_front = [[-0.383,0.001, 0.0],
@@ -61,14 +91,19 @@ class FootprintMode():
 
         self.tfBuffer = tf2_ros.Buffer()
         self.listener_tf = tf2_ros.TransformListener(self.tfBuffer)
-
-        self.client1 = dynamic_reconfigure.client.Client("/move_base/local_costmap", timeout=30)#, config_callback=dyn_cb)
-        self.client2 = dynamic_reconfigure.client.Client("/move_base/global_costmap", timeout=30)#, config_callback=dyn_cb)
+        self.cmd_sub = rospy.Subscriber('/cmd_vel', Twist, callback=self.cmd_cb)
+        self.client_local = dynamic_reconfigure.client.Client("/move_base/local_costmap", timeout=30)#, config_callback=dyn_cb)
+        self.client_global = dynamic_reconfigure.client.Client("/move_base/global_costmap", timeout=30)#, config_callback=dyn_cb)
         self.srv = Server(FootprintModeConfig, self.srv_cb)
+
+    def cmd_cb(self, cmd_vel):
+        self.driving_direction = DRIVING_DIRECTION.FORWARD if cmd_vel.linear.x >= 0 else DRIVING_DIRECTION.BACKWARD
 
     def srv_cb(self, config, level):
         self.leading_car = LEADING_CAR(config.leading_car)
         self.footprint_mode = FOOTPRINT_MODE(config.footprint_mode)
+        self.update_local_footprint = config.update_local_footprint
+        self.update_global_footprint = config.update_global_footprint
         rospy.loginfo("Received Reconfigure Requests: %s footprint and %s leading car", self.footprint_mode.name, self.leading_car.name)
         if self.leading_car == LEADING_CAR.FRONT:
             # if forward-driving: transform rear footprint into front frame
@@ -82,17 +117,15 @@ class FootprintMode():
             self.target_frame = 'axesRear'
             self.footprint_lead = self.footprint_rear
             self.footprint_follow = self.footprint_front
-
-        # when dynamic reconfigure parameter footprint_type has changed
-        # --> update footprint with static footprint of lead carriage
-        self.update_footprint(self.footprint_lead)
         return config
 
     def update_footprint(self, footprint):
         footprint_2d = footprint[:,:2] 
         footprint_2d_str = footprint_2d.tolist()
-        self.client1.update_configuration({"footprint":footprint_2d_str, "footprint_padding":self.footprint_padding})
-        self.client2.update_configuration({"footprint":footprint_2d_str, "footprint_padding":self.footprint_padding})
+        if self.update_local_footprint:
+            self.client_local.update_configuration({"footprint":footprint_2d_str, "footprint_padding":self.footprint_padding})
+        if self.update_global_footprint:
+            self.client_global.update_configuration({"footprint":footprint_2d_str, "footprint_padding":self.footprint_padding})
         return
 
     # callback for dynamic client (only needed for debugging purposes)
@@ -116,34 +149,38 @@ class FootprintMode():
 
     def main(self):    
         while not rospy.is_shutdown():
-            if self.footprint_mode == FOOTPRINT_MODE.DYNAMIC:
-                try:
-                    tf_src_targ = self.tfBuffer.lookup_transform(self.target_frame, self.source_frame, rospy.Time(0), rospy.Duration(self.period))
-                except tf2_ros.ConnectivityException:
-                    rospy.logerr('Tf tree between {:s} and {:s} is not connected'.format(self.source_frame, self.target_frame))
-                    self.rate.sleep
-                    continue
-                except tf2_ros.ExtrapolationException:
-                    rospy.logerr('Requested tf value from {:s} to {:s} is beyond extrapolation limits'.format(self.source_frame, self.target_frame))
-                    self.rate.sleep
-                    continue
-                except tf2_ros.InvalidArgumentException:
-                    rospy.logerr('Invalid arguments')
-                    self.rate.sleep
-                    continue
-                except tf2_ros.LookupException:
-                    rospy.logerr('Name of required frame is not available or broken tf tree')
-                    self.rate.sleep
-                    continue
-                except tf2_ros.TimoutException:
-                    rospy.logerr('Timeout has occured')
-                    self.rate.sleep
-                    continue
-                else:
-                    footprint_follow_wrt_lead = self.transform_point_arr(self.footprint_follow, tf_src_targ)
+            try:
+                tf_src_targ = self.tfBuffer.lookup_transform(self.target_frame, self.source_frame, rospy.Time(0), rospy.Duration(self.period))
+            except tf2_ros.ConnectivityException:
+                rospy.logerr('Tf tree between {:s} and {:s} is not connected'.format(self.source_frame, self.target_frame))
+                continue
+            except tf2_ros.ExtrapolationException:
+                rospy.logerr('Requested tf value from {:s} to {:s} is beyond extrapolation limits'.format(self.source_frame, self.target_frame))
+                continue
+            except tf2_ros.InvalidArgumentException:
+                rospy.logerr('Invalid arguments')
+                continue
+            except tf2_ros.LookupException:
+                rospy.logerr('Name of required frame is not available or broken tf tree')
+                continue
+            except tf2_ros.TimoutException:
+                rospy.logerr('Timeout has occured')
+                continue
+            else:
+                footprint_follow_wrt_lead = self.transform_point_arr(self.footprint_follow, tf_src_targ)                    
+                if self.footprint_mode == FOOTPRINT_MODE.DYNAMIC:
+                    # if front car leads: transform rear footprint into front frame and concatenate with front footprint
+                    # if rear car leads: transform front footprint into rear frame and concatenate with rear footprint
                     footprint = np.concatenate((self.footprint_lead, footprint_follow_wrt_lead))
                     self.update_footprint(footprint)
-            self.rate.sleep()
+                elif self.footprint_mode == FOOTPRINT_MODE.SEMI_STATIC and self.driving_direction == DRIVING_DIRECTION.BACKWARD:
+                    # if front car leads: transform rear footprint into front frame and only use this new rear footprint
+                    # if rear car leads: transform front footprint into rear frame and only use this new front footprint
+                    self.update_footprint(footprint_follow_wrt_lead)
+                else:
+                    self.update_footprint(self.footprint_lead)
+            finally:
+                self.rate.sleep()
 
 if __name__ == "__main__":
     rospy.init_node("robot_footprint", anonymous=True)
